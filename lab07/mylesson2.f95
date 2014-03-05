713f045281ec253ebc82acb73300b375be292be3
george <rpubaddr0@gmail.com>
2014-03-04 20:14:45 -0700

    Finished lab7 lesson2.

--- lab07/mylesson2.f95
+++ lab07/mylesson2.f95
@@ -7,9 +7,10 @@
 !  DESCRIPTION: Display corrected sensor data.
 !  VARIABLES USED:
 !    NAME:           TYPE:     COMMENT:
-!
-!
-!
+!  fix               REAL      Value to fix the data.
+!  seconds           REAL      Row 1 in the file.
+!  values            REAL      Row 2 in the file.
+!  lines             INTEGER   Lines in the data file.
 !---------------------------------------------------------------------------
 
 PROGRAM From_Here_to_There
@@ -17,11 +18,16 @@ PROGRAM From_Here_to_There
 IMPLICIT NONE  !Must explicitly declare all variables
 
 ! Declare the variables and initialize.
-    REAL :: fix
+    REAL :: fix, seconds, values
+    INTEGER :: lines
 
 ! Code here
     fix = 1.072
-
+    OPEN(1,file='inData.dat', action='read')
+    DO lines = 0, 37, 1
+        READ(1,*) seconds, values
+        WRITE(*, '(f4.2,f10.4)') seconds, values*fix
+    END DO
 
 ! Say Goodbye
    WRITE(*,*)
