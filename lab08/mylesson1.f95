a61be51d44fcb60eca737b3146fc3dd3fd6ab76c
george <rpubaddr0@gmail.com>
2014-03-19 08:15:54 -0600

    Added initial files for lab 8.

---
+++ lab08/mylesson1.f95
@@ -0,0 +1,23 @@
+!--------------------------------------------------------------------------
+!  TITLE: CS260 Lab8 Lesson 1
+!  AUTHOR: Ryan Shipp
+!  CLASS: CSCI260A
+!  DATE WRITTEN: 03.18.2014
+!  LAST REVISION: 03.18.2014
+!  DESCRIPTION: Familiarize yourself with file I/O syntax.
+!  VARIABLES USED:
+!    NAME:           TYPE:     COMMENT:
+!  
+!---------------------------------------------------------------------------
+PROGRAM Least_Squares_Lab
+IMPLICIT NONE  !Must explicitly declare all variables
+! Declare the variables and initialize.
+
+! Code here
+
+! Say Goodbye
+   WRITE(*,*)
+   WRITE(*,*) ' Have a Great Afternoon!!! '
+   WRITE(*,*)
+
+END PROGRAM
