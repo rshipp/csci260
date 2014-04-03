409da5f4b1bb4ffcb5333294fec29d9a0365a8f0
george <rpubaddr0@gmail.com>
2014-04-02 19:11:05 -0600

    Added template code for lab10.

---
+++ lab10/Lab10Lesson2.f95
@@ -0,0 +1,28 @@
+!--------------------------------------------------------------------------
+!  TITLE: CS260 Lab10 lesson 2
+!  AUTHOR: Ryan Shipp
+!  CLASS: CSCI260A
+!  DATE WRITTEN: 04.02.2014
+!  LAST REVISION: 04.02.2014
+!  DESCRIPTION: Work with two dimensional arrays in Fortran.
+!  VARIABLES USED:
+!    NAME:       TYPE:     COMMENT:
+!
+!---------------------------------------------------------------------------
+
+Program Sort_Grades
+
+Implicit None  !Must explicitly declare all variables
+
+! Declare the variables and initialize.
+
+
+! Code the Problem Here
+
+! Say Goodbye
+   Write(*,*)
+   Write(*,*) ' Have a Great Afternoon!!! '
+   Write(*,*)
+
+
+End Program   !Sort_Grades
